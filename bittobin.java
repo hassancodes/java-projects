d844951cc0a76e5c2904f00cc2cf71317579720c
hassancodes <59165844+hassancodes@users.noreply.github.com>
2021-01-30 00:38:58 -0500

    adding bittonum

---
+++ bittobin.java
@@ -0,0 +1,10 @@
+public class bittobin {
+    public static int bitToBinFunc(String bits){
+        decimalNum  = 0;
+         for(int i=0; i<bits.length();i++){
+             if(Integer.parseInt(String.ValueOf(charAtAt(i)))==1){
+
+             }
+         }
+    }
+}
