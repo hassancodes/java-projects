278477b1cf59db646e55163cc00bd85cd1e60300
hassancodes <59165844+hassancodes@users.noreply.github.com>
2021-01-31 00:01:27 -0500

    adding bit to decimal

--- bittodecimal.java
+++ bittodecimal.java
@@ -1,6 +1,15 @@
 public class bittodecimal{
-    public static int bitToDec(String bits){
+    public static char[] bitToDec(String bits){
+
+        char[] arr = new int[bits.length()];
+
+        for(int i =0; i<bits.length();i++){
+            arr[i] = bits.charAt(i);
+
+        }
 
     }
-        
+
+  return arr.toString();
+
 }
