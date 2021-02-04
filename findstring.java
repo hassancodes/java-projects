ee35aabe4f685944f59652ac6ba7bd872d366154
hassancodes <59165844+hassancodes@users.noreply.github.com>
2021-02-03 23:55:12 -0500

    getting started

--- findstring.java
+++ findstring.java
@@ -1,9 +1,15 @@
 // function to find a substring inside a given string
 public class findstring {
 
-    public String findSub(String str){
+    public String findSub(String subString, String mainString){
+
+        for(int i=0; i<mainString.length(); i++){
+            if subString.charAt(0) == mainString.charAt(i){
+                
+            }
+        }
 
     }
-    return str;
+    ;
     
 }
