4ffa968c0f62dce615a766f2084c3495df9cd21e
hassancodes <59165844+hassancodes@users.noreply.github.com>
2021-02-23 12:52:03 -0500

    commit

--- findstring.java
+++ findstring.java
@@ -3,8 +3,10 @@ public class findstring {
 
     public String findSub(String subString, String mainString){
 
+        int subCounter = 0;
+
         for(int i=0; i<mainString.length(); i++){
-            if subString.charAt(0) == mainString.charAt(i){
+            if subString.charAt(subCounter) == mainString.charAt(i){
                 
             }
         }
